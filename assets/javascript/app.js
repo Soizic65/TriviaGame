dc9bedcb0668f19ad689d02965c32a767828e430
Soizic <soizic.marie@gmail.com>
2018-10-30 17:33:46 -0500

    List questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
-    var number = 90;
+    var number = 30;
+    var incorrect = 0;
+    var correct = 0;
+    var unanswered = 0;
+
     $("#start").on("click", start);  
     
 
     function start(){
-        counter = setTimeout(timer, 1000);
+        counter = setInterval(timer, 1000);
         $("#gamePlay").show();
         $("#startgame").hide();
+        
         console.log(number);
     }       
 
     function timer(){
         number--
         $("#timer").html(number);
-        if (number === 0)
-        stop();
+        if (number === 0){
+            $("#gamePlay").hide();
+            $("#outoftime").show();
+            setTimeout(stop, 3000);
+        }
+    }
+    function stop(){
+        $("#results").show();
+        $("#outoftime").hide();
     }
 
 });
